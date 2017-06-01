6c80d7480e0e0ba152a43b52d226fa60b3a60fd3
pedrohmarques <pdhmarques@gmail.com>
2017-06-01 12:33:53 -0300

    Add files via upload

--- assets/js/tmdb.js
+++ assets/js/tmdb.js
@@ -1,7 +1,54 @@
 function filmes()
 {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.themoviedb.org/3/movie/upcoming?api_key=7c2b66d4073316cbd458dd3eb381fc17&language=pt-BR&include_image_language=pt,null", false);
+    xhr.open("GET", "https://api.themoviedb.org/3/movie/now_playing?api_key=7c2b66d4073316cbd458dd3eb381fc17&language=pt-BR&include_image_language=pt,null", false);
+    xhr.send();
+    var dadosfilmes = new Object;
+    dadosfilmes = JSON.parse(xhr.response);
+    var quant = dadosfilmes.results.length;
+	var i;
+	
+
+for (i = 1; i <= quant; i++) { 
+	var filmes = document.createElement("div");
+	filmes.setAttribute( "id","filme0" + i );
+	filmes.setAttribute( "class","filme col-sm-4" );
+	document.getElementById("banner").appendChild(filmes);
+	
+	var linkFilme = document.createElement("a");
+	linkFilme.setAttribute( "id","link0" + i );
+	linkFilme.setAttribute( "class","link" );
+	linkFilme.setAttribute( "href","InfoFilme.html?nome=" + dadosfilmes.results[i].title);
+	document.getElementById("filme0" + i).appendChild(linkFilme);
+	
+	var dadosFilmes = document.createElement("h2");
+	dadosFilmes.setAttribute( "id","nomefilme0" + i );
+	dadosFilmes.setAttribute( "class","nomefilme");
+	document.getElementById("link0"+i).appendChild(dadosFilmes);
+	
+	var dadosFilmes = document.createElement("p");
+	dadosFilmes.setAttribute( "id","sinfilme0" + i );
+	dadosFilmes.setAttribute( "class","sinfilme");
+	document.getElementById("link0"+i).appendChild(dadosFilmes);
+	
+	var dadosFilmes = document.createElement("img");
+	dadosFilmes.setAttribute( "id","imgfilme0" + i );
+	dadosFilmes.setAttribute( "class","imgfilme");
+	document.getElementById("link0"+i).appendChild(dadosFilmes);
+	
+	document.getElementById("nomefilme0"+i).innerHTML = dadosfilmes.results[i].title; "<br>";
+	document.getElementById("sinfilme0"+i).innerHTML = dadosfilmes.results[i].overview;
+	document.getElementById("imgfilme0"+i).src = "https://image.tmdb.org/t/p/w500" + dadosfilmes.results[i].poster_path;
+}
+
+
+}
+
+
+function lanAno()
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=7c2b66d4073316cbd458dd3eb381fc17&language=pt-BR&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&primary_release_year=2017", false);
     xhr.send();
     var dadosfilmes = new Object;
     dadosfilmes = JSON.parse(xhr.response);
@@ -40,9 +87,55 @@ for (i = 1; i <= quant; i++) {
 	document.getElementById("sinfilme0"+i).innerHTML = dadosfilmes.results[i].overview;
 	document.getElementById("imgfilme0"+i).src = "https://image.tmdb.org/t/p/w500" + dadosfilmes.results[i].poster_path;
 }
+	
+}
+function filmesPremiados()
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.themoviedb.org/4/list/28?page=1&api_key=7c2b66d4073316cbd458dd3eb381fc17&language=pt-BR&sort_by=release_date.desc", false);
+    xhr.send();
+    var dadosfilmes = new Object;
+    dadosfilmes = JSON.parse(xhr.response);
+    var quant = dadosfilmes.results.length;
+	var i;
+	
+
+for (i = 1; i <= quant; i++) { 
+	var filmes = document.createElement("div");
+	filmes.setAttribute( "id","filme0" + i );
+	filmes.setAttribute( "class","filme" );
+	document.getElementById("banner").appendChild(filmes);
+	
+	var linkFilme = document.createElement("a");
+	linkFilme.setAttribute( "id","link0" + i );
+	linkFilme.setAttribute( "class","link" );
+	linkFilme.setAttribute( "href","InfoFilme.html?nome=" + dadosfilmes.results[i].title);
+	document.getElementById("filme0" + i).appendChild(linkFilme);
+	
+	var dadosFilmes = document.createElement("h2");
+	dadosFilmes.setAttribute( "id","nomefilme0" + i );
+	dadosFilmes.setAttribute( "class","nomefilme");
+	document.getElementById("link0"+i).appendChild(dadosFilmes);
+	
+	var dadosFilmes = document.createElement("p");
+	dadosFilmes.setAttribute( "id","sinfilme0" + i );
+	dadosFilmes.setAttribute( "class","sinfilme");
+	document.getElementById("link0"+i).appendChild(dadosFilmes);
+	
+	var dadosFilmes = document.createElement("img");
+	dadosFilmes.setAttribute( "id","imgfilme0" + i );
+	dadosFilmes.setAttribute( "class","imgfilme");
+	document.getElementById("link0"+i).appendChild(dadosFilmes);
+	
+	document.getElementById("nomefilme0"+i).innerHTML = dadosfilmes.results[i].title; "<br>";
+	document.getElementById("sinfilme0"+i).innerHTML = dadosfilmes.results[i].overview;
+	document.getElementById("imgfilme0"+i).src = "https://image.tmdb.org/t/p/w500" + dadosfilmes.results[i].poster_path;
+}
+	
 
 
 }
+
 
 function queryObj() {
     var result = {}, keyValuePairs = location.search.slice(1).split("&");
